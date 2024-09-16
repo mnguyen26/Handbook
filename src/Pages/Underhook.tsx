8fc3fa4885b9a1343cf2ee4722cc69892bab467f
Michael <michael.m.nguyen@live.com>
2024-09-15 23:11:54 -0500

    underhook content

--- src/Pages/Underhook.tsx
+++ src/Pages/Underhook.tsx
@@ -8,7 +8,8 @@ export const underhookSections: Section[] = [
     children: [
         {
             title: "Setup",
-            content: [``],
+            content: [`A simple way to enter on an underhook is with a snap. Take a hard club down and towards the side that you want your underhook, and on the way back up dig your underhook. 
+                Once you have a single underhook established you can look for a variety of grips on the other side including wrist control, a collar tie, double unders etc. `],
             ref: "underhookSetupRef",
             video: "HR0gmDmtwCI?start=0"
         },
@@ -19,19 +20,24 @@ export const underhookSections: Section[] = [
             children: [
                 {
                     title: "Knee Tap",
-                    content: [``],
+                    content: [`The knee tap is a highly reliable takedown and the mechanics are very simple: block your opponent's knee while you push them through that direction. 
+                        Its simplicity can sometimes make it obvious so it often requires other takedown sequences to set it up. One thing to consider at a more advanced level is 
+                        what leg you first step forward with as you begin to run your feet. If you have an opponent who has a dangerous whizzer kick/uchi mata you might want to 
+                        avoid stepping first with your outside leg because it gives them space to step in front of you and attempt a throw. Against someone who is very competent 
+                        at upper body throws they can even lat drop you. If this concerns you, step forward first with your inside foot or the one that is closer to your opponent 
+                        so that it's more difficult for them to step in front of you.`],
                     ref: "kneeTapRef",
                     video: "HR0gmDmtwCI?start=74"
                 },
                 {
                     title: "High Crotch",
-                    content: [``],
+                    content: [`The underhook allows you to shoot a high-crotch on the same side. It also allows you to first pull them into the stance required to shoot your shot.`],
                     ref: "highCrotchRef",
                     video: "HR0gmDmtwCI?start=116"
                 },
                 {
                     title: "Single Leg",
-                    content: [``],
+                    content: [`The underhook also allows you to shoot a single-leg on the same side. Similarly, you should use it to pull your opponent into the stance that is required to shoot.`],
                     ref: "singleLegRef",
                     video: "HR0gmDmtwCI?start=140"
                 },
@@ -40,35 +46,41 @@ export const underhookSections: Section[] = [
         {
             title: "Takedowns: Throws & Foot Techniques",
             content: [``],
-            ref: "throwsref",
+            ref: "",
             children: [
                 {
                     title: "Hip Throw (O Goshi)",
-                    content: [``],
+                    content: [`This is the basic hip throw or o goshi. I believe this is the first throw people should learn as it's easiest to feel and learn the positioning and leverage required for throws.
+                         Your positioning should place you close to your opponent's center of gravity so that you can lift them off the mat wit your legs. It is basic but it's immediately obvious how powerful it is. You will rarely hit this on someone trained but the threat of it is important for setting up other techniques.`],
                     ref: "hipThrowRef",
                     video: "HR0gmDmtwCI?start=160"
                 },
                 {
                     title: "Pinch Headlock Tai Otoshi",
-                    content: [``],
+                    content: [`The Tai Otoshi is not considered a hip throw but rather a hand technique in judo. While the gi provides much more powerful grips to off-balance and manipulate your opponent,
+                         taking a pinch headlock offers a very close alternative in no-gi. Use this powerful grip to pull and twist your opponent over your leg. Point your leg away from your opponent so 
+                         that there is no lateral stress on the knee. `],
                     ref: "pinchHeadlockRef",
                     video: "HR0gmDmtwCI?start=216"
                 },
                 {
                     title: "Hip Throw Snap",
-                    content: [``],
+                    content: [`Even if you you can't fully throw your opponent on an attempt you can often get them off balance enough to lower their head. This is a good opportunity to look for a snap-down
+                         and you'll have many options from the front headlock.`],
                     ref: "hipThrowSnapRef",
                     video: "HR0gmDmtwCI?start=271"
                 },
                 {
                     title: "Ouchi Kouchi Knee Tap",
-                    content: [``],
+                    content: [`This knee tap setup makes use of the threat of a couple of trips. You first threaten the inside trip so that your opponent has to move that leg out of the way which commits their weight to the opposite leg. 
+                        You then block that opposite leg with a kouchi gari. In the moment that they are unable to move their leg out of the way, place your hand behind their knee and run your knee tap.`],
                     ref: "ouchiKouchiRef",
                     video: "HR0gmDmtwCI?start=316"
                 },
                 {
                     title: "Ouchi Sasae Foot Sweep",
-                    content: [``],
+                    content: [`This foot sweep is set up using the threat of an inside trip or throw on the same side as the underhook. The goal is to get your opponent to step back very high so that almost all their weight is 
+                        committed to one leg. They become much easier to off-balance and manipulate in this moment when they are on one leg and you can use that opportunity to hit a foot sweep.`],
                     ref: "ouchiSasaeRef",
                     video: "HR0gmDmtwCI?start=355"
                 },
@@ -77,17 +89,19 @@ export const underhookSections: Section[] = [
         {
             title: "Takedowns: Misc",
             content: [``],
-            ref: "miscRef",
+            ref: "",
             children: [
                 {
                     title: "Throw By",
-                    content: [``],
+                    content: [`The throw by can be used to quickly position yourself behind your opponent. It can present itself when they are pressing too hard into you, in between sequences when they haven't yet established a strong whizzer, or
+                         or simply when they are occupied with other attacks. It's important to always be aware of how your opponent is defending your underhook. If for a moment you feel like you can bypass their upper body defense, go for the 
+                         throw by. Once behind your opponent you have a variety of mat returns to choose from.`],
                     ref: "throwByRef",
                     video: "HR0gmDmtwCI?start=31"
                 },
                 {
                     title: "Snap Down",
-                    content: [``],
+                    content: [`If your opponent is overtly avoiding upper body engagement by sticking their butt away from you and lowering their head, you can attempt a snap-down and then choose how to attack next from the front headlock.`],
                     ref: "snapDownRef",
                     video: "HR0gmDmtwCI?start=410"
                 },
@@ -96,23 +110,25 @@ export const underhookSections: Section[] = [
         {
             title: "Takedowns: Double Unders",
             content: [``],
-            ref: "miscRef",
+            ref: "",
             children: [
                 {
                     title: "Bear Hug",
-                    content: [``],
+                    content: [`The "bear hug" or "back breaker" is a simple takedown from double underhooks from the front. Pinch your double under grip right behind their spine at about waist level 
+                        and drive them forward with your head. `],
                     ref: "bearHugRef",
                     video: "HR0gmDmtwCI?start=436"
                 },
                 {
                     title: "Power Twist",
-                    content: [``],
+                    content: [`If you are more side on to your opponent with double unders, you can attempt a power twist to off balance them or even take them all the way to the mat. `],
                     ref: "powerTwistRef",
                     video: "HR0gmDmtwCI?start=497"
                 },
                 {
                     title: "Front to Back",
-                    content: [``],
+                    content: [`This is a highly reliable sequence that first starts with a power twist. If your opponent reacts by stepping in front of you in order to avoid getting thrown, 
+                        you can then pull them back either blocking them with your leg or with a big mat return.`],
                     ref: "frontToBackRef",
                     video: "HR0gmDmtwCI?start=535"
                 }
