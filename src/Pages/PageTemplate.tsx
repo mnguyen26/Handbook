6ec9bf7a27e795d1047240ca2c0c10ca033260ad
Michael <michael.m.nguyen@live.com>
2024-09-20 10:06:24 -0500

    fix video size for mobile

--- src/Pages/PageTemplate.tsx
+++ src/Pages/PageTemplate.tsx
@@ -118,8 +118,6 @@ const VideoThumbnail = (props: VideoThumbnailProps) => {
               onClick={(e) => e.stopPropagation()}
             >
               <iframe
-                width="560"
-                height="315"
                 src={`https://www.youtube.com/embed/${props.videoId}`}
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
