c8c2d7e92fc797e9a7f069d98bf08e9c78aac80c
Michael <michael.m.nguyen@live.com>
2024-09-21 19:32:40 -0500

    about

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -5,11 +5,10 @@ export const aboutSections: Section[] = [
   { 
     title: "About the Grappler's Handbook", 
     level: 1, 
-    content: [`The Grapplers Handbook is my effort to create an open educational resource for the jiu jitsu and grappling community. This material is designed as a quick reference for practitioners at the white to blue belt level (0-4 years of experience), though there's likely something of value here for everyone. The content is drawn from my own 
-            experiences over the years; things I've learned from my coaches, training partners, seminars, instructional materials I've purchased and stolen, and my own 
-            experimentation in training and competition. I, myself, am continually learning and our sport is continually evolving so the content will constantly be changing
-            accordingly. I hope this resource is useful to you as an informational tool, but remember, the best way to learn and improve is to get on the mats and train. 
-            Get started by searching for something in the table of contents.`],
+    content: [`Gym memberships are expensive. Gis and uniforms are expensive. Tournaments are expensive. And hopefully, you haven't experienced this yet, but hospital visits are also expensive. While it's reasonable to attach costs to goods and services, I don't want access to educational and instructional material to be another financial barrier to our sport.`,
+      `The Grapplers Handbook is my effort to create an open educational resource for the jiujitsu and grappling community. The material is designed as a quick reference for practitioners from white to blue belt (0-4 years of experience) but there is probably something of value here for everyone. The content is drawn from my own experience over the years—things I've learned from coaches, training partners, seminars, instructionals, and my own experimentation in training and competition. Jiujitsu is a continually evolving sport, and I'm continually learning, so the content here will constantly be changing accordingly.`,
+      `Much like Wikipedia, it can only serve as an introduction to various topics and not as a comprehensive final point of reference. I hope you find this resource useful as an informational tool, but remember, the best way to learn and improve is to get on the mats and train. Get started by searching for something in the table of contents.`,
+      ],
     ref: "aboutRef",
     component: () => <Quotes />
   }
